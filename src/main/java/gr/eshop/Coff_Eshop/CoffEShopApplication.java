e2521d32bb31a69154ec1256857eef8935c5c989
john <giannovios@hotmail.com>
2019-12-30 22:16:26 +0200

    Initial Commit : Test git and upload the app on github

---
+++ src/main/java/gr/eshop/Coff_Eshop/CoffEShopApplication.java
@@ -0,0 +1,13 @@
+package gr.eshop.Coff_Eshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffEShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoffEShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gr/eshop/Coff_Eshop/CoffEShopApplicationTests.java
@@ -0,0 +1,13 @@
+package gr.eshop.Coff_Eshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffEShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
